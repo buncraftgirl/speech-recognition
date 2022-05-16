4f6be9f9d2c0691621c2a3344dcd96e3bf19a767
buncraftgirl <87542372+buncraftgirl@users.noreply.github.com>
2022-05-16 17:01:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+function() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    classifier = ml5.imageClassifier('MobileNet',modelLoaded);
+}
+
+function modelLoaded() {
+    console.log('Model Loaded!');
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    classifier.classify(video, gotResult);
+}
+var previous_result = '';
+function gotresult(error, result) {
+    if (error) {
+        console.error(error);
+    }else{
+        if((results[0].confiidence > 0.5) && (previous_result != results[0].label)){
+       console.log(results);
+       previous_result = results[0].label;
+       var synth = window.speechSynthesis;
+       speak
+        }
+    }
+}
